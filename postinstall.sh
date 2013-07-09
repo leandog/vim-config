7dcde142294daa0cb7ba834623cbeb42513022f5
Eric Hankinson <eric.hankinson@leandog.com>
2013-07-09 13:04:11 -0400

    Some errors with Vundle loading initially

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-curl -s https://raw.github.com/bronson/vim-update-bundles/master/vim-update-bundles | ruby
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/ackrc ~/.ackrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+curl -s https://raw.github.com/bronson/vim-update-bundles/master/vim-update-bundles | ruby
