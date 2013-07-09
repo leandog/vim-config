b0daed67e391800f93b1083ea57db467423d2b69
Eric Hankinson <eric.hankinson@leandog.com>
2013-07-09 14:13:26 -0400

    Further automates the install

--- postinstall.sh
+++ postinstall.sh
@@ -3,4 +3,5 @@
 ln -s ~/.vim/vimrc ~/.vimrc
 ln -s ~/.vim/ackrc ~/.ackrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-curl -s https://raw.github.com/bronson/vim-update-bundles/master/vim-update-bundles | ruby
+echo -n "\n\n***  Launching vim and installing the Bundles ***\n\n"
+vim +BundleInstall +qall
